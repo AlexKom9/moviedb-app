d1d6df3c11a68bb6d07e574e0e74510669048efd
Alexandr Komar <909komar@gmail.com>
2018-09-30 16:59:05 +0300

    make Header stateLess component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,20 @@
 import React, { Component } from "react";
 import Login from "./Login/Login";
-import User from "./User"
-import HeaderMenu from './HeaderMenu'
-import PropTypes from "prop-types";
+import User from "./User";
 
-class Header extends Component {
-  // static propTypes = {
-  //   user: PropTypes.isRequired
-  // };
-
-  render() {
-    const { updateUser, updateSessionId, user } = this.props;
-    // console.log(User)
-    return (
-      <nav className="navbar navbar-dark bg-primary">
-        <div className="container">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a href="#" className="nav-link">
-                Home
-              </a>
-            </li>
-          </ul>
-          {user ? <User/>: <Login/> }
-        </div>
-      </nav>
-    );
-  }
-}
-
-Header.propTypes = {};
+const Header = ({user}) => (
+  <nav className="navbar navbar-dark bg-primary">
+    <div className="container">
+      <ul className="navbar-nav">
+        <li className="nav-item active">
+          <a href="#" className="nav-link">
+            Home
+          </a>
+        </li>
+      </ul>
+      {user ? <User /> : <Login />}
+    </div>
+  </nav>
+);
 
 export default Header;

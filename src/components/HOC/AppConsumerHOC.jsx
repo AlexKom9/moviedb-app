4ccf0fde755049ee4bc9d429283084ed486c8003
Alexandr Komar <909komar@gmail.com>
2018-09-30 14:38:21 +0300

    add spread this.props

--- src/components/HOC/AppConsumerHOC.jsx
+++ src/components/HOC/AppConsumerHOC.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { AppContext } from "../App";
 
 export default Component =>
-  class AppContextConsumer extends React.Component {
+  class AppConsumerHOC extends React.Component {
     render() {
       return (
         <AppContext.Consumer>
           {context => (
             <Component
+              {...this.props}
               session_id={context.session_id}
               updateSessionId={context.updateSessionId}
               user={context.user}

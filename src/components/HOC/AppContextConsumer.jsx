02561e9615fbef5e84ccd7b9722ed4ec5a198f0b
Alexandr Komar <909komar@gmail.com>
2018-09-30 14:30:35 +0300

    add HOC AppContextConsumer

--- src/components/HOC/AppContextConsumer.jsx
+++ src/components/HOC/AppContextConsumer.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import { AppContext } from "../App";
 
-const AppContextConsumer = Component => {
-  return (
-    <AppContext.Consumer>
-      {context => (
-        <Component
-          session_id={context.session_id}
-          updateSessionId={context.updateSessionId}
-          user={context.user}
-          updateUser={context.updateUser}
-        />
-      )}
-    </AppContext.Consumer>
-  );
-};
+export default Component =>
+  class AppContextConsumer extends React.Component {
+    render() {
+      return (
+        <AppContext.Consumer>
+          {context => (
+            <Component
+              session_id={context.session_id}
+              updateSessionId={context.updateSessionId}
+              user={context.user}
+              updateUser={context.updateUser}
+            />
+          )}
+        </AppContext.Consumer>
+      );
+    }
+  };
 
-AppContextConsumer.displayName = "AppContextConsumer";
+// const AppContextConsumer = Component => {
+//   console.log(AppContext);
+//   return (
+//     <AppContext.Consumer>
+//       {context => (
+//         <Component
+//           session_id={context.session_id}
+//           updateSessionId={context.updateSessionId}
+//           user={context.user}
+//           updateUser={context.updateUser}
+//         />
+//       )}
+//     </AppContext.Consumer>
+//   );
+// };
 
-export default AppContextConsumer;
+// AppContextConsumer.displayName = "AppContextConsumer";
+//
+// export default AppContextConsumer;

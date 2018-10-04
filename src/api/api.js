d1c1a0067b3196ffd7fbd3fde5312f100857cd4c
Alexandr Komar <909komar@gmail.com>
2018-10-04 23:44:47 +0300

    add get and post method to CallApi

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,11 @@
-export const API_URL = 'https://api.themoviedb.org/3';
+import queryString from "query-string";
 
-export const API_KEY_3 = '942fe0c9aeb6728941df8e74f3fbce09';
+export const API_URL = "https://api.themoviedb.org/3";
 
-export const API_KEY_4 = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NDJmZTBjOWFlYjY3Mjg5NDFkZjhlNzRmM2ZiY2UwOSIsInN1YiI6IjViOTk2ZmU5OTI1MTQxN2YwZjAwNDRjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DyQ5EAhU4gfsNyNTnPRqayFOn-SAy61d7iWwfWhOkkw';
+export const API_KEY_3 = "942fe0c9aeb6728941df8e74f3fbce09";
 
+export const API_KEY_4 =
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NDJmZTBjOWFlYjY3Mjg5NDFkZjhlNzRmM2ZiY2UwOSIsInN1YiI6IjViOTk2ZmU5OTI1MTQxN2YwZjAwNDRjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DyQ5EAhU4gfsNyNTnPRqayFOn-SAy61d7iWwfWhOkkw";
 
 export const fetchApi = (url, options = {}) => {
   return new Promise((resolve, reject) => {
@@ -25,3 +27,46 @@ export const fetchApi = (url, options = {}) => {
       });
   });
 };
+
+export default class CallApi {
+  static get(url, options = {}) {
+    const { params = {} } = options;
+    const queryStringParams = {
+      api_key: API_KEY_3,
+      language: "ru-RU",
+      ...params
+    };
+    // url = /discover/movie?
+    // params = {
+    //
+    // }
+    return fetchApi(
+      `${API_URL}${url}${queryString.stringify(queryStringParams)}`,
+      {
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json"
+        }
+      }
+    );
+  }
+  static post(url, options = {}) {
+    const { params = {}, body={} } = options;
+    const queryStringParams = {
+      api_key: API_KEY_3,
+      language: "ru-RU",
+      ...params
+    };
+    return fetchApi(
+      `${API_URL}${url}${queryString.stringify(queryStringParams)}`,
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(body)
+      }
+    );
+  }
+}

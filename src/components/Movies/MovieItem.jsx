c3ede706310323bda75b7b7964de4af9b8a1e5bb
Alexandr Komar <909komar@gmail.com>
2018-10-06 11:22:06 +0300

    start make favor and watchlist

--- src/components/Movies/MovieItem.jsx
+++ src/components/Movies/MovieItem.jsx
@@ -1,24 +1,71 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import AppConsumerHOC from "../HOC/AppConsumerHOC";
+import CallApi from '../../api/api'
 
-export default class MovieItem extends React.Component {
+class MovieItem extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      booked: false,
+      favor: false
+    };
+    this.changeFavor = this.changeFavor.bind(this);
+    this.changeBooked = this.changeBooked.bind(this);
+  }
+
+  changeFavor() {
+    const { session_id } = this.props;
+
+    if (!session_id){
+    }
+
+    this.setState({
+      favor: !this.state.favor
+    });
+
+
+
+  }
+
+  changeBooked() {
+    this.setState({
+      booked: !this.state.booked
+    });
+  }
+
+
+
+
   render() {
-    const { item } = this.props;
+    const { item, session_id } = this.props;
+    console.log(session_id);
     return (
-      <div className="card" style={{ width: '100%' }}>
+      <div className="card" style={{ width: "100%" }}>
         <img
           className="card-img-top card-img--height"
-          src={`https://image.tmdb.org/t/p/w500${item.backdrop_path
-            || item.poster_path}`}
+          src={`https://image.tmdb.org/t/p/w500${item.backdrop_path ||
+            item.poster_path}`}
           alt=""
         />
         <div className="card-body">
           <h6 className="card-title">{item.title}</h6>
           <div className="card-text">
-Рейтинг:
+            Рейтинг:
             {item.vote_average}
+          </div>
+
+          <div className="movie-item__favor" onClick={this.changeFavor}>
+            <FontAwesomeIcon icon={["fas", "heart"]} />
+          </div>
+
+          <div className="movie-item__bookmaktr" onClick={this.changeBooked}>
+            <FontAwesomeIcon icon={["fas", "bookmark"]} />
           </div>
         </div>
       </div>
     );
   }
 }
+
+export default AppConsumerHOC(MovieItem);

845047c6ac09931cf7aba9b9779f9547e3678832
Alexandr Komar <909komar@gmail.com>
2018-10-06 15:29:07 +0300

    add method getFevorMovies and getWatchList from API

--- src/components/Movies/MoviesList/MoviesHOC.jsx
+++ src/components/Movies/MoviesList/MoviesHOC.jsx
@@ -2,67 +2,119 @@ import React from "react";
 import _ from "lodash";
 import PropTypes from "prop-types";
 import CallApi from "../../../api/api";
+import AppConsumerHOC from "../../HOC/AppConsumerHOC";
 
 export default Component =>
-  class MoviesHOC extends React.Component {
-    constructor() {
-      super();
-
-      this.state = {
-        movies: []
-      };
-    }
-
-    static propTypes = {
-      filters: PropTypes.object.isRequired,
-      getTotalPages: PropTypes.func.isRequired,
-      changePage: PropTypes.func.isRequired,
-      page: PropTypes.number.isRequired
-    };
-
-    getMovies = (filters, page) => {
-      const { getTotalPages } = this.props;
-      const { sort_by, primary_release_year, with_genres } = filters;
-
-      let genreString = "";
-
-      for (let i = 0; i < with_genres.length; i++) {
-        genreString += `&with_genres=${with_genres[i]}`;
+  AppConsumerHOC(
+    class MoviesHOC extends React.Component {
+      constructor() {
+        super();
+        this.state = {
+          movies: [],
+          favor_movies: [],
+          watch_list: []
+        };
       }
-      //TODO: Add with_genres query param
 
-      const queryStringParams = {
-        sort_by,
-        language: "ru-RU",
-        page,
-        primary_release_year
+      static propTypes = {
+        filters: PropTypes.object.isRequired,
+        getTotalPages: PropTypes.func.isRequired,
+        changePage: PropTypes.func.isRequired,
+        page: PropTypes.number.isRequired
       };
-      CallApi.get(`/discover/movie?`, { params: queryStringParams }).then(
-        data => {
-          getTotalPages(data.total_pages);
-          this.setState({
-            movies: data.results
-          });
+
+      getMovies = (filters, page) => {
+        const { getTotalPages } = this.props;
+        const { sort_by, primary_release_year, with_genres } = filters;
+
+        let genreString = "";
+
+        for (let i = 0; i < with_genres.length; i++) {
+          genreString += `&with_genres=${with_genres[i]}`;
         }
-      );
-    };
+        //TODO: Add with_genres query param
+        const queryStringParams = {
+          sort_by,
+          language: "ru-RU",
+          page,
+          primary_release_year
+        };
 
-    componentDidMount() {
-      this.getMovies(this.props.filters, this.props.page);
-    }
+        CallApi.get(`/discover/movie?`, { params: queryStringParams }).then(
+          data => {
+            getTotalPages(data.total_pages);
+            this.setState({
+              movies: data.results
+            });
+          }
+        );
+      };
 
-    componentDidUpdate(prevProps) {
-      if (!_.isEqual(this.props.filters, prevProps.filters)) {
-        this.props.changePage(1);
-        this.getMovies(this.props.filters, 1);
-      }
-      if (this.props.page !== prevProps.page) {
+      getFavorMovies = () => {
+        const queryStringParams = {
+          session_id: this.props.session_id
+        };
+        CallApi.get("/account/{account_id}/favorite/movies?", {
+          params: queryStringParams
+        }).then(data => {
+          this.setState({
+            favor_movies: data.results
+          });
+        });
+      };
+
+      getWatchList = () => {
+        const queryStringParams = {
+          session_id: this.props.session_id
+        };
+        CallApi.get("/account/{account_id}/watchlist/movies?", {
+          params: queryStringParams
+        }).then(data => {
+          this.setState({
+            watch_list: data.results
+          });
+        });
+      };
+
+      componentDidMount() {
         this.getMovies(this.props.filters, this.props.page);
+        if (this.props.session_id) {
+          this.getFavorMovies();
+          this.getWatchList();
+        }
+      }
+
+      componentDidUpdate(prevProps) {
+        if (!_.isEqual(this.props.filters, prevProps.filters)) {
+          this.props.changePage(1);
+          this.getMovies(this.props.filters, 1);
+        }
+        if (this.props.page !== prevProps.page) {
+          this.getMovies(this.props.filters, this.props.page);
+        }
+        //TODO: optimize
+        if (
+          prevProps.session_id !== this.props.session_id &&
+          this.props.session_id
+        ) {
+          this.getFavorMovies();
+          this.getWatchList();
+        }
+      }
+
+      render() {
+        const { movies, favorite_movies, watch_list } = this.state;
+        // console.log(this.props);
+        console.log("render MoviesHOC");
+        return (
+          <Component
+            {...this.props}
+            // session_id={context.session_id}
+            movies={movies}
+            favorite_movies={favorite_movies}
+            watch_list={watch_list}
+          />
+        );
       }
     }
-
-    render() {
-      const { movies } = this.state;
-      return <Component movies={movies} />;
-    }
-  };
+  );

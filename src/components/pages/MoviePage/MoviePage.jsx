a2e1be550aaadd0ac3dc1c22c09a2910bf7aefe0
Alexandr Komar <909komar@gmail.com>
2018-10-06 18:04:37 +0300

    add layouts for MoviePage

--- src/components/pages/MoviePage/MoviePage.jsx
+++ src/components/pages/MoviePage/MoviePage.jsx
@@ -2,12 +2,51 @@ import React from "react";
 import CallApi from "../../../api/api";
 
 export default class MoviePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: null
+    };
+  }
+
   componentDidMount() {
-    CallApi.get(`/movie/${this.props.match.params.id}?`);
+    CallApi.get(`/movie/${this.props.match.params.id}?`).then(data => {
+      console.log(data);
+      this.setState({
+        movie: data
+      });
+    });
   }
 
   render() {
     console.log(this.props);
-    return <div>MoviePage</div>;
+    const { movie } = this.state;
+    return (
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-4">
+            <img
+              className="card-img-top card-img--height"
+              src={`https://image.tmdb.org/t/p/w500${(movie &&
+                movie.backdrop_path) ||
+                (movie && movie.poster_path)}`}
+              alt=""
+            />
+          </div>
+          <div className="col-8">
+            <h2 className="title mb-4">{movie && movie.title}</h2>
+            <p className="mb-4">{movie && movie.overview}</p>
+            <div>
+              <button type="button" className="btn btn-primary">
+                Like
+              </button>
+              {/*<button type="button" className="btn btn-secondary">*/}
+                {/*Secondary*/}
+              {/*</button>*/}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }

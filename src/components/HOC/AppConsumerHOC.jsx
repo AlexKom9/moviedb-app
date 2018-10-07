bf18f08e53da6c6f592b038eac8618f8ae529b33
Alexandr Komar <909komar@gmail.com>
2018-10-07 17:11:02 +0300

    refactor AppConsumerHOC

--- src/components/HOC/AppConsumerHOC.jsx
+++ src/components/HOC/AppConsumerHOC.jsx
@@ -9,11 +9,18 @@ export default Component =>
           {context => (
             <Component
               {...this.props}
-              session_id={context.session_id}
-              updateSessionId={context.updateSessionId}
-              user={context.user}
-              updateUser={context.updateUser}
-              favorite_movies={context.favorite_movies}
+              {...context}
+              // session_id={context.session_id}
+              // updateSessionId={context.updateSessionId}
+              // user={context.user}
+              // updateUser={context.updateUser}
+              // updateAuth={context.updateAuth}
+              // isAuth={context.isAuth}
+              // favorite_movies={context.favorite_movies}
+              // logOut={context.logOut}
+              // showLoginForm={context.showLoginForm}
+              // toggleLoginForm={context.toggleLoginForm}
+              // hideLoginForm={context.hideLoginForm}
             />
           )}
         </AppContext.Consumer>

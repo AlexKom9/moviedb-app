9ec1fbbf61a43a59303cc2bb2430a40c1fcbefd5
Alexandr Komar <909komar@gmail.com>
2018-10-12 18:16:37 +0300

    add like and toWatchlist to moviePage

--- src/components/pages/MoviePage/MoviePage.jsx
+++ src/components/pages/MoviePage/MoviePage.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import Like from '../../Movies/Markers/Like'
+import ToWatch from '../../Movies/Markers/ToWatch'
 import CallApi from "../../../api/api";
+import markHOC from "../../Movies/Markers/markHOC";
 
 export default class MoviePage extends React.Component {
   constructor(props) {
@@ -19,17 +22,20 @@ export default class MoviePage extends React.Component {
   }
 
   render() {
+    const MovieLike = markHOC(Like, 'favorite_movies');
+    const MovieToWatch = markHOC(ToWatch, 'watchlist');
     console.log(this.props);
     const { movie } = this.state;
+    console.log(movie);
     return (
       <div className="container">
-        <div className="row mt-4">
+        {movie && <div className="row mt-4">
           <div className="col-4">
             <img
               className="card-img-top card-img--height"
               src={`https://image.tmdb.org/t/p/w500${(movie &&
                 movie.backdrop_path) ||
-                (movie && movie.poster_path)}`}
+              (movie && movie.poster_path)}`}
               alt=""
             />
           </div>
@@ -37,15 +43,17 @@ export default class MoviePage extends React.Component {
             <h2 className="title mb-4">{movie && movie.title}</h2>
             <p className="mb-4">{movie && movie.overview}</p>
             <div>
-              <button type="button" className="btn btn-primary">
-                Like
-              </button>
-              {/*<button type="button" className="btn btn-secondary">*/}
-                {/*Secondary*/}
-              {/*</button>*/}
+              <div className="movie-item__like float-right ml-2">
+                <MovieToWatch id={movie.id} />
+              </div>
+
+              <div className="movie-item__like float-right">
+                <MovieLike id={movie.id} />
+              </div>
             </div>
           </div>
-        </div>
+        </div>}
+
       </div>
     );
   }

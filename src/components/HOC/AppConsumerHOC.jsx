07bf7d7718418d876549f8b134e5f5fbc6c2740f
Alexandr Komar <909komar@gmail.com>
2018-10-12 08:45:27 +0300

    small fix

--- src/components/HOC/AppConsumerHOC.jsx
+++ src/components/HOC/AppConsumerHOC.jsx
@@ -10,17 +10,6 @@ export default Component =>
             <Component
               {...this.props}
               {...context}
-              // session_id={context.session_id}
-              // updateSessionId={context.updateSessionId}
-              // user={context.user}
-              // updateUser={context.updateUser}
-              // updateAuth={context.updateAuth}
-              // isAuth={context.isAuth}
-              // favorite_movies={context.favorite_movies}
-              // logOut={context.logOut}
-              // showLoginForm={context.showLoginForm}
-              // toggleLoginForm={context.toggleLoginForm}
-              // hideLoginForm={context.hideLoginForm}
             />
           )}
         </AppContext.Consumer>

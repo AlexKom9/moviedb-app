c54d8a6dbb2e4ae5482d9b75a773cb17bc8e1517
Alexandr Komar <909komar@gmail.com>
2018-10-19 00:20:05 +0300

    add props col form MovieList

--- src/components/Movies/MoviesList/MoviesList.jsx
+++ src/components/Movies/MoviesList/MoviesList.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import MovieItem from "../MovieItem";
 import PropTypes from "prop-types";
 
-const MovieList = ({ movies = [] }) => {
+const MovieList = ({ movies = [], col = 'col-6' }) => {
   return (
     <div className="row">
       {movies.map(movie => (
-        <div key={movie.id} className="col-6 mb-4">
+        <div key={movie.id} className={`${col} mb-4`}>
           <MovieItem
             item={movie}
           />

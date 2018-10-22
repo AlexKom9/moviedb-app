5c2b448b6871e0c9b7f4539307f0e70c257b232c
Alexandr Komar <909komar@gmail.com>
2018-10-22 11:34:12 +0300

    small fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Login from "./Login/Login";
 import User from "./User";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
-const Header = ({user}) => (
+const Header = ({ user }) => (
   <nav className="navbar">
     <div className="container">
       <ul className="navbar-nav">
         <li className="nav-item active">
-            <Link className="nav-link header__home-link" to="/">Home</Link>
+          <Link className="nav-link header__home-link" to="/">
+            Home
+          </Link>
         </li>
       </ul>
       {user ? <User /> : <Login />}

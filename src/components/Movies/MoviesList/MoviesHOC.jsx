c5505bce56242317437b26435901d5f444901df7
Alexandr Komar <909komar@gmail.com>
2018-10-24 19:45:28 +0300

    add Loader

--- src/components/Movies/MoviesList/MoviesHOC.jsx
+++ src/components/Movies/MoviesList/MoviesHOC.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as actions from "../../../actions/actions";
+import Loader from '../../Loader'
 
 const mapStateToProps = ({ movies }) => {
   return {
@@ -42,7 +43,7 @@ export default Component =>
 
       render() {
         return this.props.isFetching ? (
-          <h1>isFetching</h1>
+          <Loader/>
         ) : (
           <Component
           {...this.props}

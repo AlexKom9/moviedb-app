781b3f449194acaf73cdf9b928fabcf210b49aa4
Alexandr Komar <909komar@gmail.com>
2018-11-01 11:30:56 +0200

    add pagination to SimilarMovies

--- src/components/pages/MoviePage/SimilarMovies.jsx
+++ src/components/pages/MoviePage/SimilarMovies.jsx
@@ -2,37 +2,83 @@ import React, { Component } from "react";
 import CallApi from "../../../api/api";
 import MoviesList from "../../Movies/MoviesList/MoviesList";
 import { withRouter } from "react-router-dom";
+import SimilarMoviesPaginatorHOC from "../../HOC/SimilarMoviesPaginatorHOC";
+import Paginator from "../../Filters/Paginator";
+import Loader from "../../Loader";
+
+const SimilarMoviesPaginator = SimilarMoviesPaginatorHOC(Paginator);
 
 class SimilarMovies extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movies: []
+      movies: [],
+      total_pages: null,
+      total_results: null,
+      page: null,
+      isFetching: false
     };
   }
 
-  componentDidMount() {
+  onChangePage = page => {
+    page =
+      page >= 0
+        ? page <= this.state.total_pages
+          ? page
+          : this.state.total_pages
+        : 0;
+    this.setState(
+      {
+        page
+      },
+      () => {
+        this.getMovies(page);
+      }
+    );
+  };
+
+  getMovies = (page = 1) => {
     const {
       match: {
         params: { id }
       }
     } = this.props;
     const queryStringParams = {
-      page: 1
+      page
     };
+    this.setState({
+      isFetching: true
+    });
     CallApi.get(`/movie/${id}/similar`, {
       params: queryStringParams
     }).then(data => {
       this.setState({
-        movies: data.results
+        movies: data.results,
+        total_pages: data.total_pages,
+        total_results: data.total_results,
+        page: data.page,
+        isFetching: false
       });
     });
+  };
+
+  componentDidMount() {
+    this.getMovies();
   }
 
   render() {
+    const { movies, isFetching, page, total_pages } = this.state;
     return (
       <div className="mt-4">
-        <MoviesList movies={this.state.movies} col="col-4" />
+        <SimilarMoviesPaginator
+          onChangePage={this.onChangePage}
+          page={page}
+          total_pages={total_pages}
+          mode="inline"
+        />
+        <div className="mt-4">
+          {isFetching ? <Loader/> : movies.length ? <MoviesList movies={movies} col="col-4" /> : <p className="text-center">Нет похожих фильмов</p>}
+        </div>
       </div>
     );
   }

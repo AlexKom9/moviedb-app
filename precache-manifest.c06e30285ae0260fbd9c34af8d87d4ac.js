9e6c70a54807dca7933eb8a3c108de6b6ee2f27f
Alexandr Komar <909komar@gmail.com>
2018-11-01 17:12:07 +0200

    Updates

---
+++ precache-manifest.c06e30285ae0260fbd9c34af8d87d4ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e81c94a62ef82b98eada",
+    "url": "/moviedb-app/static/js/runtime~main.e81c94a6.js"
+  },
+  {
+    "revision": "49b8de5a05ee2fc82e60",
+    "url": "/moviedb-app/static/js/main.49b8de5a.chunk.js"
+  },
+  {
+    "revision": "b47277b4763fca470bf6",
+    "url": "/moviedb-app/static/js/1.b47277b4.chunk.js"
+  },
+  {
+    "revision": "49b8de5a05ee2fc82e60",
+    "url": "/moviedb-app/static/css/main.ef88bc47.chunk.css"
+  },
+  {
+    "revision": "b47277b4763fca470bf6",
+    "url": "/moviedb-app/static/css/1.f5709737.chunk.css"
+  },
+  {
+    "revision": "8f60e125c611e47760294ddc7507c685",
+    "url": "/moviedb-app/index.html"
+  }
+];
